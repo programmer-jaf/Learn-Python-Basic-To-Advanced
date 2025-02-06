81a6a34d1ed15d6fd82914236d220dc8f1828706
Programmer-Jaf <programmerjaf@gmail.com>
2025-02-06 22:28:25 +0600

    Add Chapter 04: Introduction to operators in Python with examples

---
+++ Chapter_04/operators.py
@@ -0,0 +1,67 @@
+print("operators in python")
+
+# There are 4 types of operators in Python
+'''
+1. Arithmetic Operator ➡️ +,-,*,/,% etc
+2. Assignment Operator ➡️ =,+=,-=,*=,/= etc
+3. Comperison Operator
+4. Logical Operator
+'''
+# Arithmetic operator
+
+a = 10
+b = 20
+
+# Addition, subtraction, multiplication, division, and modulus operations are performed on the operands.
+print(a+b) # 30
+print(a-b) # -10
+print(a*b) # 200
+print(a/b) # 0.5
+print(a%b) # 10
+
+# Assignment Operator
+# Equals to (=)
+a = 10
+# Plus Equals to (+=)
+a += 10
+print(a) # 20
+
+# Minus Equals to (-=)
+a -= 10
+print(a) # 10
+
+# Multiply Equals to (*=)
+a *= 10
+print(a) # 100
+
+# Division Equals to (/=)
+a /= 10
+print(a) # 10.0
+
+
+# Comparison Operator
+a = 10
+b = 20
+
+# Equality operator (==) compares the values of two operands and returns True if they are equal.
+print(a == b) # False
+
+# Not equal operator (!=) compares the values of two operands and returns True if they are not equal.
+print(a != b) # True
+
+# Greater than operator (>) compares the values of two operands and returns True if the left operand is greater than the right operand.
+print(a > b) # False
+# Less than operator (<) compare the values of two operands and returns True if the right operand is greater than
+print(a < b) # True
+print(a >= b) # False
+print(a <= b) # True
+
+
+# Logical Operator
+
+a = True
+b = False
+print(a and b) # False
+print(a or b) # True
+print(not a) # False
+
---
+++ Chapter_04/readme.md
@@ -0,0 +1,10 @@
+# Operators in Python
+
+## There are 4 types of operators in Python
+
+```txt
+1. Arithmetic Operator ➡️ +,-,*,/,% etc
+2. Assignment Operator ➡️ =,+=,-=,*=,/= etc
+3. Comparison Operator ➡️ >,<,>=,<=,==,===,!= etc
+4. Logical Operator ➡️ and, or , not
+```
