689405e0e22b37a68af59b91bc00230fd2451dcb
Programmer-Jaf <programmerjaf@gmail.com>
2025-02-07 11:04:33 +0600

    Add Chapter 05: Introduction to type casting in Python with examples

---
+++ Chapter_05/type__casting.py
@@ -0,0 +1,57 @@
+my__name = "Programmer Jaf"
+
+# to check the data-type
+print(type(my__name))
+
+# to access the attribute
+
+print(my__name)
+
+# to change the value
+
+my__name = "New Programmer Jaf"
+
+print(my__name)
+
+# to delete the attribute
+
+del my__name
+
+# print(my__name)
+
+# Python does not support private attributes or methods in the same way as other languages like Java or C++.
+
+
+# type-casting
+a = 34
+print(a)
+print(type(a))
+# change the integer to string
+
+a = str(a)
+
+print(a)
+
+print(type(a))
+
+# change the string to float
+b = "354"
+
+print(b)
+print(type(b))
+
+# change string to float
+b = float(b)
+print(b)
+print(type(b))
+
+# change the float to integer
+c = 5.4624
+print(c)
+print(type(c))
+
+# change float to integer
+
+c = int(c)
+print(c)
+print(type(c))
